e3dbb996ee790af47f2090ddca7f38187f952b6d
aidanaMergem <aidanamergembaeva@gmail.com>
2023-11-25 12:18:07 +0600

    Adding first model

--- src/main/java/com/busticketbooking/Application.java
+++ src/main/java/com/busticketbooking/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BusTicketBookingApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(BusTicketBookingApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
