e8434c369b0f49c2c2785b502bfa5f1c545b4df6
aidanaMergem <aidanamergembaeva@gmail.com>
2024-01-19 09:38:40 +0600

    ticket

--- src/main/java/com/busticketbooking/model/Ticket.java
+++ src/main/java/com/busticketbooking/model/Ticket.java
@@ -1,4 +1,18 @@
 package com.busticketbooking.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Ticket {
+    long id;
+    Date depDate;
+
 }
